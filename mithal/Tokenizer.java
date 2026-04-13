6fc4dc44ac7b6e0a8eb95286ae32101110432254
Selim Elkaffas <selimkaffas@gmail.com>
2026-04-13 08:08:59 +0200

    Added support for single and double char operators, and whitespace

--- mithal/Tokenizer.java
+++ mithal/Tokenizer.java
@@ -48,10 +48,95 @@ class Tokenizer {
     }
 
     private void scanToken() {
-        
+        char c = advance();
+        switch (c) {
+            case '(': addToken(TokenType.LEFT_PAREN); break;
+            case ')': addToken(TokenType.RIGHT_PAREN); break;
+            case '{': addToken(TokenType.LEFT_BRACE); break;
+            case '}': addToken(TokenType.RIGHT_BRACE); break;
+            case ',': addToken(TokenType.COMMA); break;
+            case '.': addToken(TokenType.DOT); break;
+            case '-': addToken(TokenType.MINUS); break;
+            case '+': addToken(TokenType.PLUS); break;
+            case ';': addToken(TokenType.SEMICOLON); break;
+            case '*': addToken(TokenType.STAR); break;
+
+            case '!': 
+                addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG); 
+                break;
+            case '=': 
+                addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); 
+                break;
+            case '<': 
+                addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS); 
+                break;
+            case '>': 
+                addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); 
+                break;
+            case '/':
+                if (match('/')) {
+                    // A comment goes until the end of the line/file.
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(TokenType.SLASH);
+                }
+                break;
+
+            case ' ':
+            case '\r': 
+            case '\t':
+                break;
+
+            case '\n':
+                line++;
+                break;
+
+            case '"': string(); break;
+
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Mithal.error(line, "Unexpected character.");
+                }
+                break; 
+        }
     }
 
     private boolean isAtEnd() {
         return current >= source.length();  
+    }
+
+    private char advance() {
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true; 
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean isDigit(char c) {
+        if (c >= '0' && c <= '9') return true;
     }
 }
