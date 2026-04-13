e34c0cbc690d49ee97b8044cbcaa6e262fa14f21
Selim Elkaffas <selimkaffas@gmail.com>
2026-04-13 15:52:40 +0200

    Finished tokenizer

--- mithal/Tokenizer.java
+++ mithal/Tokenizer.java
@@ -1,9 +1,11 @@
 package mithal;
 
+import java.lang.String;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 class Tokenizer {
     private final String source;
@@ -16,21 +18,21 @@ class Tokenizer {
     private static final Map<String, TokenType> keywords;
     static {
         keywords = new HashMap<>();
-        keywords.put("and", TokenType.AND);
-        keywords.put("else", TokenType.ELSE);
-        keywords.put("false", TokenType.FALSE);
-        keywords.put("for", TokenType.FOR);
-        keywords.put("fun", TokenType.FUN);
-        keywords.put("if", TokenType.IF);
-        keywords.put("nil", TokenType.NIL);
-        keywords.put("or", TokenType.OR);
-        keywords.put("print", TokenType.PRINT);
-        keywords.put("return", TokenType.RETURN);
-        keywords.put("super", TokenType.SUPER);
-        keywords.put("this", TokenType.THIS);
-        keywords.put("true", TokenType.TRUE);
-        keywords.put("var", TokenType.VAR);
-        keywords.put("while", TokenType.WHILE);
+        keywords.put("و", TokenType.AND);
+        keywords.put("أو", TokenType.OR);
+        keywords.put("إذ", TokenType.IF);
+        keywords.put("وإلا", TokenType.ELSE);
+        keywords.put("حق", TokenType.TRUE);
+        keywords.put("باطل", TokenType.FALSE);
+        keywords.put("لكل", TokenType.FOR);
+        keywords.put("طالما", TokenType.WHILE);
+        keywords.put("دالة", TokenType.FUN);
+        keywords.put("ارجع", TokenType.RETURN);
+        keywords.put("صافر", TokenType.NIL);
+        keywords.put("قل", TokenType.PRINT);
+        keywords.put("الأصل", TokenType.SUPER);
+        keywords.put("فرعي", TokenType.THIS);
+        keywords.put("متغ", TokenType.VAR);
     }
 
     Tokenizer(String source) {
@@ -54,11 +56,11 @@ class Tokenizer {
             case ')': addToken(TokenType.RIGHT_PAREN); break;
             case '{': addToken(TokenType.LEFT_BRACE); break;
             case '}': addToken(TokenType.RIGHT_BRACE); break;
-            case ',': addToken(TokenType.COMMA); break;
+            case '،': addToken(TokenType.COMMA); break;
             case '.': addToken(TokenType.DOT); break;
             case '-': addToken(TokenType.MINUS); break;
             case '+': addToken(TokenType.PLUS); break;
-            case ';': addToken(TokenType.SEMICOLON); break;
+            case '؛': addToken(TokenType.SEMICOLON); break;
             case '*': addToken(TokenType.STAR); break;
 
             case '!': 
@@ -119,6 +121,11 @@ class Tokenizer {
         return source.charAt(current);
     }
 
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
     private boolean match(char expected) {
         if (isAtEnd()) return false;
         if (source.charAt(current) != expected) return false;
@@ -137,6 +144,54 @@ class Tokenizer {
     }
 
     private boolean isDigit(char c) {
-        if (c >= '0' && c <= '9') return true;
+        // TODO: Write proper regex
+        return Pattern.matches("[0-9٠١٢٣٤٥٦٧٨٩]", String.valueOf(c));
+    }
+
+    private boolean isAlpha(char c) {
+        // TODO: Write proper regex
+        return Pattern.matches("[ًٌَُّْدجحخهعغفقثصضطنتالبيسشظزوةىلارؤءئ]", String.valueOf(c));
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isDigit(c) || isAlpha(c);
+    }
+
+    // TODO: Disallow numbers that mix between Eastern and Western Arabic numerals
+    private void number() {
+        while(!isDigit(peek())) advance();
+
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+            while(!isDigit(peek())) advance();
+        }
+
+        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void string() {
+        while(peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Mithal.error(line, "سلسلة بتراء"); // Arabic for "Prematurely terminated string"
+            return;
+        }
+
+        advance();
+
+        String value = source.substring(start + 1, current - 1);
+        addToken(TokenType.STRING, value);
+    }
+
+    private void identifier() {
+        while(isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = TokenType.IDENTIFIER;
+        addToken(type);
     }
 }
