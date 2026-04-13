3d74ee02aac77eb2db675c65df1e25323955b710
Selim Elkaffas <selimkaffas@gmail.com>
2026-04-13 07:47:43 +0200

    Added REPL

---
+++ mithal/Mithal.java
@@ -0,0 +1,63 @@
+package mithal;
+
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import java.util.List;
+
+public class Mithal {
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: mithal [script]"); 
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError) System.exit(65);
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        Tokenizer tokenizer = new Tokenizer(source);
+        Token[] tokens = tokenizer.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.out.println("[line " + line + "] Error " + where + ": " + message);
+        hadError = true;
+    }
+}
